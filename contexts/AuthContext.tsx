158344b5bf831ed74022691c75b099fc27c6a3ea
rafaelcastan <rc.castanheira190@gmail.com>
2021-11-04 15:32:17 -0300

    add SSR authentication

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,101 @@
+import Router from "next/router";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies, destroyCookie } from 'nookies'
+import { api } from "../services/apiClient";
+import router from "next/router";
+
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[];
+}
+
+type SignInCredentials = {
+    email: string;
+    password: string;
+}
+
+type AuthContextData = {
+    signIn(credentials : SignInCredentials) : Promise<void>;
+    user : User;
+    isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+    children : ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut(){
+    destroyCookie(undefined, 'nextauth.token')
+    destroyCookie(undefined, 'nextauth.refreshToken')
+
+    router.push('/')
+}
+
+export function AuthProvider({children}:AuthProviderProps) {
+    const [user, setUser] = useState<User>({} as User);
+    
+    const isAuthenticated = !!user.email;
+
+    useEffect(()=> {
+        const {'nextauth.token': token }  = parseCookies()
+
+        if (token) {
+            api.get('/me').then(response =>{
+                const { email, permissions, roles } = response.data
+
+                setUser({
+                    email,
+                    permissions,
+                    roles,
+                })
+            })
+            .catch(( ) =>{
+                signOut();
+            })
+        }
+    }, [])
+
+
+    async function signIn({email,password}:SignInCredentials){
+        try{
+            const response = await api.post('sessions', {
+                email,
+                password
+            })
+
+            const { token, refreshToken, permissions, roles} = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, //30 days
+                path: '/'
+            })
+            
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, //30 days
+                path: '/'
+            })
+    
+            setUser({
+                email,
+                permissions,
+                roles,
+            })
+
+            api.defaults.headers[`Authorization`] = `Bearer ${token} `;
+
+            Router.push('/dashboard');
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <AuthContext.Provider value = {{signIn, isAuthenticated, user}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
---
+++ pages/dashboard.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthContext"
+import { setupAPIClient } from "../services/api"
+import { api } from "../services/apiClient"
+import { withSSRAuth } from "../utils/withSSRAuth"
+
+export default function Dashboard() {
+    const {user} = useContext(AuthContext)
+
+    return(
+        <h1>Dashboard: {user?.email}</h1>
+    )
+}
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get('/me');
+
+    console.log(response.data)
+
+    return {
+      props: {}
+    }
+})
